f84f8ea30a8006f428ea6ab975c30b9cac89a68f
Sregey Slezhov <pishuhott@gmail.com>
2021-03-03 11:58:13 +0300

    fix: cklick buttons

--- js/main.js
+++ js/main.js
@@ -1,59 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
     const keys = document.querySelectorAll('.key');
-    let audios = document.querySelectorAll('audio');
 
-
-    // function playSound(e) {
-    //     const audio = document.querySelector(`audio[data-key='${e.keyCode}']`),
-    //         key = document.querySelector(`.key[data-key='${e.keyCode}']`);
-    //     if (!audio) return;
-
-    //     audio.currentTime = 0;
-    //     audio.play();
-    //     key.classList.add('playing');
-    // }
-
-    function removeTransition(e) {
-        if (e.propertyName !== 'transform') return;
-        this.classList.remove('playing');        
-    }
-
-    keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-
-    window.addEventListener('keydown', function (e) {
-        const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
-        key = document.querySelector(`.key[data-key='${e.keyCode}']`);
+    function playSound(code) {
+        const audio = document.querySelector(`audio[data-key='${code}']`);
+        const key = document.querySelector(`.key[data-key='${code}']`);
         if (!audio) return;
 
         audio.currentTime = 0;
         audio.play();
         key.classList.add('playing');
+        console.log(key);
+    }
 
-        console.log(audio);
+    function removeTransition(e) {
+        if (e.propertyName !== 'transform') return;
+        this.classList.remove('playing');
+    }
+
+    keys.forEach(key => key.addEventListener('click', function (e) {
+        this.getAttribute('data-key')
+        playSound(this.getAttribute('data-key'))
+        console.log(this.getAttribute('data-key'))
+    }));
+
+    keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+    window.addEventListener('keydown', function (e) {
+        playSound(e.keyCode);
     });
-
-    let audiosAll = function () {
-        for (let i = 0; i < audios.length; i++) {
-            let audio = audios[i];
-            console.log(audio);
-        };
-    };
-
-    
-    
-    for (let i = 0; i < keys.length; i++) {
-        let key = keys[i];
-        key.addEventListener('click', function (e) {
-            // const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
-
-
-            console.log(audios{e.keyCode});
-        });
-    };
-
-
-
-
-
 
 });
